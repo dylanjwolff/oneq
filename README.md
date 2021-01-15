d137d6a87518fee34d8963cbc360543019010ff8
wolffd <wolffdy0@gmail.com>
2021-01-15 04:42:31 +0100

    project setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # oneq
+An attempt to make a task management app that I actually like
---
+++ android/app/src/main/kotlin/com/example/oneq/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.oneq
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
